6ea51e4bc8399f98886ca67adc4d1f741495a2e2
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-03-15 06:08:40 +0000

    Getter and Setter for marks

--- Student.java
+++ Student.java
@@ -19,4 +19,12 @@
      public void setName(String name) {
          this.name = name;
      }
+
+     public int getMarks() {
+         return marks;
+     }
+     public void setMarks(int marks) {
+         this.marks = marks;
+         
+     }
  }
