a8358a8b597980936b5d886e6f8700a91afe8553
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-03-15 06:06:13 +0000

    Constructor for Student

--- Student.java
+++ Student.java
@@ -7,4 +7,9 @@
  class Student {
      private String name;
      private int marks;
+
+     public Student(String name, int marks) {
+         this.name = name;
+         this.marks = marks;
+     }
  }
