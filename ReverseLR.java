a20c51d77f7ae7679c04029a1ae8a0211a8a6ba7
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-03-15 06:03:17 +0000

    fix: correct loop logic to reverse left and right halves properly

--- ReverseLR.java
+++ ReverseLR.java
@@ -5,21 +5,28 @@
  
  input 13
  output 6 5 4 3 2 1 7 13 12 11 10 9 8*/
- 
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class ReverseLR {
-     public static void main(String[] args) { 
-         Scanner sc = new Scanner(System.in); 
-         int n = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
         
-        for (int i = n / 2; i >= 1; i--) { 
-             System.out.print(i + " "); 
-         } 
-
-         for (int i = (n / 2) + 1; i <= n; i++) { 
-             System.out.print(i + " "); 
-         } 
- 
-     }
- }
+        
+        for (int i = n / 2; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        
+        
+        if (n % 2 != 0) {
+            System.out.print((n / 2) + 1 + " ");
+        }
+        
+        
+        for (int i = n; i > (n / 2) + (n % 2); i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
