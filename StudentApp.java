fdb24c2f7da1de01f7e59a99a6f25093a570bfd3
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-03-15 06:12:42 +0000

    Create student objects

--- StudentApp.java
+++ StudentApp.java
@@ -1,5 +1,11 @@
 public class StudentApp {
      public static void main(String[] args) {
          ArrayList<Student> students = new ArrayList<>();
+
+         students.add(new Student("Chamika", 85));
+         students.add(new Student("Sanduni", 78));
+         students.add(new Student("Lahiru", 91));
+         students.add(new Student("Samanmali", 72));
+         students.add(new Student("Tharushi", 88));
      }
  }
