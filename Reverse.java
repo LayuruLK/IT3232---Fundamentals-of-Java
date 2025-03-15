9c218a4e5f88ebbd47411127cb809367188a18ac
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-03-15 05:45:37 +0000

    Question 03 - Print the numbers in reverse

---
+++ Reverse.java
@@ -0,0 +1,12 @@
+//Print the numbers in reverse
+ public class Reverse{
+ 	public static void main(String args[]){
+ 		int num = 12345678;
+ 		
+ 		while(num>0){
+ 			int n1 = num%10;
+ 			System.out.println(n1);
+ 			num=num/10;
+ 		}
+ 	}
+ }
