551357ca0d936c412886ea5f81ddeb313db5423b
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-03-15 06:05:09 +0000

    Question 06 - Create Student class

---
+++ Student.java
@@ -0,0 +1,10 @@
+/*Create a student class
+ 	-create 5 student objects and store it in a proper datastructure
+     -find the average marks of the students*/
+ 
+ import java.util.ArrayList;
+ 
+ class Student {
+     private String name;
+     private int marks;
+ }
